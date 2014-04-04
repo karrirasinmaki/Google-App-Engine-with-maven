ee693cd62435b978e72ec0b99cb9f55bb7a0226e
karrirasinmaki <karri.rasinmaki@gmail.com>
2014-04-04 14:23:01 +0300

    New template

--- README.md
+++ README.md
@@ -10,15 +10,3 @@ Download and install maven:
 http://maven.apache.org/  
 
 
-Templates
----------
-####testapp 
-*skeleton-archetype*  
-Empty skeleton with very minimal depedencies  
-**build**
-perform these commands:  
-```
-mvn clean install
-mvn -pl myproject-ear appengine:devserver
-```
-Your dev server will be running at `http://localhost:8080/`
---
+++ appenginemaven/src/main/java/fi/raka/appenginemaven/DemoServlet.java
@@ -0,0 +1,16 @@
+package fi.raka.appenginemaven;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DemoServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    
+      resp.getWriter().println("Hello Maven!");
+  }
+}
